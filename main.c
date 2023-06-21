2edfe8d6d57895b56a9cc401c4fb454b0b87166a
wert_3000_ultra <mtruttse@gmail.com>
2023-06-21 22:53:20 +0300

    ex 2 and ex 3

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("i am happy to live in you\n");
-    printf("i hope i stay here\n");
-    printf("thanks\n");
+    printf("1\n");
+    printf("4\n");
+    printf("9\n");
+    printf("16\n");
+    printf("25\n");
+    printf("36\n");
+    printf("49\n");
+    printf("64\n");
+    printf("81\n");
+    printf("100\n");
     return 0;
 }
