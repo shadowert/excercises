c6636e8d1dbd3ba2c800563de7d179f05f566d0c
wert_3000_ultra <mtruttse@gmail.com>
2023-06-22 13:59:35 +0300

    ex 4 father

--- main.c
+++ main.c
@@ -3,15 +3,16 @@
 
 int main()
 {
-    printf("1\n");
-    printf("4\n");
-    printf("9\n");
-    printf("16\n");
-    printf("25\n");
-    printf("36\n");
-    printf("49\n");
-    printf("64\n");
-    printf("81\n");
-    printf("100\n");
+
+    for (int n=0;  n <10; n++) {
+        printf( "%d\n", n*n);
+
+    }
+    for (int y=10;  y <20; y++) {
+        printf( "%d\n", y*y*y);
+
+    }
+
+
     return 0;
 }
